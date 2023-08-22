953ca132a6c962610006e6dfbece17fbbb7c55f5
Wagner Terry <goncalveswagner15@gmail.com>
2023-08-22 18:54:51 -0300

    feat: configurando conexão com o banco mongoDB Atlas

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express'; 'express'
 import express from 'express';
 const apiService = require('./src/service/APIService');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+//const { MongoClient, ServerApiVersion } = require('mongodb');
+
 // import apiService from './src/service/APIService'
 // middleware
 const app = express()
@@ -21,5 +26,17 @@ app.get('/launches', async (req: Request, res: Response) => {
     }
 })
 
-// Server
-app.listen(3002, () => console.log("Servidor rodando na porta 3002"))
+// conexão com o mongoDB Atlas
+const PORT = process.env.PORT || 3003
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.j3kkis4.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log("Conectado ao MongoDB Atlas na porta: ", PORT)
+        app.listen(PORT)
+    })
+    .catch((error: string) => {
+        console.log("erro ao conectar ao banco.", error)
+    })
