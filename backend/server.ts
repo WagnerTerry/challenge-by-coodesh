dbf11df6a490526074d28faef6efc39c0b06f25a
Wagner Terry <goncalveswagner15@gmail.com>
2023-08-25 09:41:11 -0300

    refactor: Refatorando código para a pasta routes

--- backend/server.ts
+++ backend/server.ts
@@ -1,87 +1,17 @@
-import { Request, Response } from 'express'; 'express'
 import express from 'express';
 
-const apiService = require('./src/service/APIService');
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-
 dotenv.config()
+
+const launch = require('./src/routes/launch')
 
 // middleware
 const app = express()
 app.use(express.json())
 
-let savedData: any = null;
-
-
 // routes
-const BASEURL = 'https://api.spacexdata.com/v5/launches'
-app.get('/', (req: Request, res: Response) => {
-    res.status(200).send({ message: "Fullstack Challenge 🏅 - Space X API" })
-})
-
-app.get('/launches', async (req: Request, res: Response) => {
-    try {
-        const apiData = await apiService.fetchDataFromAPI(BASEURL);
-
-        const page: number = parseInt(req.query.page as string, 10) || 1; // Página padrão é 1
-        const limit: number = parseInt(req.query.limit as string, 10) || 4; // Limite padrão é 4
-        const itemName = req.query.name || ''; // Nome padrão é vazio
-
-        const filteredItems = apiData.filter(item => item.name.includes(itemName));
-
-        const totalItems: number = filteredItems.length;
-        const totalPages: number = Math.ceil(totalItems / limit); // Arredondamento para cima
-
-        const startIndex = (page - 1) * limit;
-        const endIndex = page * limit;
-
-        const paginatedItems = filteredItems.slice(startIndex, endIndex);
-
-        const hasNext: boolean = page < totalPages;
-        const hasPrevious: boolean = page > 1;
-
-        const totalDocs = paginatedItems.length * totalPages
-
-        savedData = {
-            results: paginatedItems,
-            totalDocs,
-            page,
-            totalPages,
-            hasNext,
-            hasPrevious
-        }
-
-        res.json({
-            results: paginatedItems,
-            totalDocs,
-            page,
-            totalPages,
-            hasNext,
-            hasPrevious
-        });
-
-    } catch (error) {
-        res.status(400).json({ "message": "Error message" })
-    }
-})
-
-app.get('/launches/stats', async (req: Request, res: Response) => {
-    const launchResult = {};
-
-    const allLaunches = await apiService.fetchDataFromAPI(BASEURL);
-    const countLaunch = allLaunches.map((launch) => launch.success)
-    countLaunch.forEach(item => {
-        if (launchResult[item] !== undefined) {
-            return launchResult[item]++;
-        } else {
-            return launchResult[item] = 1;
-        }
-    })
-    const rocket = allLaunches.map((launch) => { return { name: launch.name, rocket: launch.rocket, reused: launch.cores[0].reused } })
-
-    res.status(200).json({ launchResult, rocket })
-})
+app.use("/", launch)
 
 // conexão com o mongoDB Atlas
 const PORT = process.env.PORT || 3003
---
+++ backend/src/routes/launch.ts
@@ -0,0 +1,82 @@
+import { Request, Response } from 'express';
+import express from 'express';
+import { ParsedQs } from 'qs';
+
+const apiService = require('../service/APIService');
+
+// middleware
+const router = express()
+router.use(express.json())
+
+let savedData: any = null;
+
+// routes
+const BASEURL = 'https://api.spacexdata.com/v5/launches'
+router.get('/', (req: Request, res: Response) => {
+    res.status(200).send({ message: "Fullstack Challenge 🏅 - Space X API" })
+})
+
+router.get('/launches', async (req: Request, res: Response) => {
+    try {
+        const apiData = await apiService.fetchDataFromAPI(BASEURL);
+
+        const page: number = parseInt(req.query.page as string, 10) || 1; // Página padrão é 1
+        const limit: number = parseInt(req.query.limit as string, 10) || 4; // Limite padrão é 4
+        const itemName = req.query.name || ''; // Nome padrão é vazio
+
+        const filteredItems = apiData.filter((item: { name: (string | ParsedQs | string[] | ParsedQs[])[]; }) => item.name.includes(itemName));
+
+        const totalItems: number = filteredItems.length;
+        const totalPages: number = Math.ceil(totalItems / limit); // Arredondamento para cima
+
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+
+        const paginatedItems = filteredItems.slice(startIndex, endIndex);
+
+        const hasNext: boolean = page < totalPages;
+        const hasPrevious: boolean = page > 1;
+
+        const totalDocs = paginatedItems.length * totalPages
+
+        savedData = {
+            results: paginatedItems,
+            totalDocs,
+            page,
+            totalPages,
+            hasNext,
+            hasPrevious
+        }
+
+        res.json({
+            results: paginatedItems,
+            totalDocs,
+            page,
+            totalPages,
+            hasNext,
+            hasPrevious
+        });
+
+    } catch (error) {
+        res.status(400).json({ "message": "Error message" })
+    }
+})
+
+router.get('/launches/stats', async (req: Request, res: Response) => {
+    const launchResult: { [key: string]: number } = {};
+
+    const allLaunches = await apiService.fetchDataFromAPI(BASEURL);
+    const countLaunch = allLaunches.map((launch: { success: any; }) => launch.success)
+    countLaunch.forEach((item: string | number) => {
+        if (launchResult[item] !== undefined) {
+            return launchResult[item]++;
+        } else {
+            return launchResult[item] = 1;
+        }
+    })
+    const rocket = allLaunches.map((launch: { name: any; rocket: any; cores: { reused: any; }[]; }) => { return { name: launch.name, rocket: launch.rocket, reused: launch.cores[0].reused } })
+
+    res.status(200).json({ launchResult, rocket })
+})
+
+module.exports = router
