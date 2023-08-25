252b14a1b51aa54914d057ed09599efc251789f5
Wagner Terry <goncalveswagner15@gmail.com>
2023-08-25 17:25:03 -0300

    chore: adicionando tabela e campo de busca

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import BarChart from "./components/BarChart"
 import "./App.scss"
 
 const dataPizza = [
-  { name: 'Sucesso', value: 150 },
+  { name: 'Sucesso 8', value: 150 },
   { name: 'Falha', value: 40 },
   { name: 'Nulo', value: 10 },
 ];
@@ -33,8 +33,42 @@ function App() {
           <div className="barchart">
             <h2>Bar Chart</h2>
             <BarChart data={barChartData} />
-
           </div>
+        </div>
+
+        <div className="fetch-rocket">
+          <h2>Registros de lançamentos</h2>
+          <div className="search">
+            <input type="search" placeholder="Search here" />
+            <button type="submit">Buscar</button>
+          </div>
+        </div>
+
+        <div className="flights">
+          <table>
+            <thead>
+              <tr>
+                <th>N Vôo</th>
+                <th>Logo</th>
+                <th>Missão</th>
+                <th>Data de lançamento</th>
+                <th>Foguete</th>
+                <th>Resultado</th>
+                <th>Vídeo</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="Nome do Produto">a</td>
+                <td data-label="Código do Produto">a</td>
+                <td data-label="Descrição">a</td>
+                <td data-label="Unidades">a</td>
+                <td data-label="Valor Custo">a</td>
+                <td data-label="Categoria">a</td>
+                <td data-label="Marca">a</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
 
       </main>
